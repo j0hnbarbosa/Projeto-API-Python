cd6b7db61abade31ec91433d6ef0406b2c578be5
j0hnbarbosa <john.contatob@gmail.com>
2019-11-27 00:22:12 -0300

    Adicionado endpoints

--- app.py
+++ app.py
@@ -1,15 +1,32 @@
-from src.CRUD.crud import cadastrarCliente
+from src.CRUD.crud import cadastrarCliente, criaBases, verificaLogin, cadastrarUsuario, recuperarSenha_verificaEmail
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/login', methods=['POST'])
-def criaCliente():
+criaBases()
+
+@app.route('/cadastrarCliente', methods=['POST'])
+def cadastraCliente():
   cadastrarCliente(request.json)
   print('VALOR Aqui {}'.format(request.json))
   return jsonify(request.json)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+  verificaLogin(request.json)
+  return 'sucesso'
+
+@app.route('/cadastrarUsuario', methods=['POST'])
+def cadastraUsuario():
+  cadastrarUsuario(request.json)
+  return 'sucesso - cadastrarUsuario'
+
+@app.route('/recuperarSenhaVerificaEmail', methods=['GET'])
+def recuperarSenhaVerificaEmail():
+  recuperarSenha_verificaEmail(request.json)
+  return 'sucesso - recuperarSenhaVerificaEmail'
 
 #Quando estiver usando o app.run(debug=True) execute usando 'python app.py'
 if __name__ == '__main__':
--- src/CRUD/crud.py
+++ src/CRUD/crud.py
@@ -6,14 +6,16 @@ mycursor = mydb.cursor()
 print(mydb)
 
 
-# Cria base de dados caso nao exista
+# ========= Cria base de dados caso nao exista =================
 def criaBases():
   try:
-      mycursor.execute("Create DATABASE traba_db")
+      mycursor.execute("create DATABASE traba_db")
+      tbl_usuario()
       tbl_cliente()
       tbl_veiculo()
       tbl_dono_veiculo()
       print("Database Criada Com sucesso!")
+      # terminaConexao()
   except:
     print("Database já existe!") 
 
@@ -21,7 +23,7 @@ def criaBases():
 def tbl_cliente():
   try:
     mycursor.execute(
-      "CREATE TABLE `traba_db`.`tbl_cliente` ( `id` INT NOT NULL AUTO_INCREMENT , `nome` VARCHAR(60) NOT NULL , `sobrenome` VARCHAR(60) NOT NULL , `endereco` VARCHAR(100) NOT NULL , `telefone` VARCHAR(15) NOT NULL , `cpf` VARCHAR(11) NOT NULL , PRIMARY KEY (`id`)) "
+      "CREATE TABLE `traba_db`.`tbl_cliente` ( `id` INT NOT NULL AUTO_INCREMENT , `nome` VARCHAR(60) NOT NULL , `sobrenome` VARCHAR(60) NOT NULL , `endereco` VARCHAR(100) NOT NULL , `telefone` VARCHAR(15) NOT NULL , `cpf` VARCHAR(11) NOT NULL, `email` VARCHAR(60) NOT NULL, `senha` VARCHAR(60) NOT NULL, PRIMARY KEY (`id`)) "
       )
     print('Tabela criada com sucess!')
   except:
@@ -45,13 +47,22 @@ def tbl_dono_veiculo():
   except:
     print("Tabela já criada.")
 
+def tbl_usuario():
+  try:
+    mycursor.execute(
+      "CREATE TABLE `traba_db`.`tbl_usuario` ( `id` INT NOT NULL AUTO_INCREMENT , `nome` VARCHAR(60) NOT NULL , `email` VARCHAR(60) NOT NULL , `senha` VARCHAR(60) NOT NULL , PRIMARY KEY (`id`))"
+      )
+    print('Tabela criada com sucess!')
+  except:
+    print("Tabela já criada.")
+
+# ========= Manipula as tabelas =================
 
 try:
-  def cadastrarCliente(clienteNovo):
+  def cadastrarCliente(valores):
 
-    nome, sobrenome, endereco, telefone, cpf = list(clienteNovo.values())
-    sql = "INSERT INTO `traba_db`.`tbl_cliente` (`nome`, `sobrenome`, `endereco`, `telefone`, `cpf`) VALUES(%s, %s, %s, %s, %s)"
-    vlr = (nome, sobrenome, endereco, telefone, cpf)
+    sql = "INSERT INTO `traba_db`.`tbl_cliente` (`nome`, `sobrenome`, `endereco`, `telefone`, `cpf`, `email`, `senha`) VALUES(%s, %s, %s, %s, %s, %s, %s)"
+    vlr = (valores['nome'], valores['sobrenome'], valores['endereco'], valores['telefone'], valores['cpf'], valores['email'], valores['senha'])
     print("clienteNovo: {}".format(vlr))
 
     mycursor.execute(sql, vlr)
@@ -62,26 +73,69 @@ except:
   print('Dados não inseridos')
 
 try:
-  def cadastrarVeiculo(veiculoNovo):
-    marca, fabricante, ano, chassis, cor = list(veiculoNovo.values())
+  def cadastrarVeiculo(valores):
+    marca, fabricante, ano, chassis, cor = list(valores.values())
     sql = "INSERT INTO `traba_db`.`tbl_veiculo` (`marca`, `fabricante`, `ano`, `chassis`, `cor`) VALUES(%s, %s, %s, %s, %s)"
     vlr = (marca, fabricante, ano, chassis, cor)
 
     mycursor.execute(sql, vlr)
+    terminaConexao()
+    print("Dados criados com sucesso")
+except:
+  print('Dados não inseridos')
+
+try:
+  def cadastrarDonoVeiculo(valores):
+    idCliente, idViculo = list(valores.values())
+    sql = "INSERT INTO `traba_db`.`tbl_dono_veiculo` (`idCliente`, `idVeiculo`) VALUES(%s, %s)"
+    vlr = (idCliente, idViculo)
+
+    mycursor.execute(sql, vlr)
+    terminaConexao()
 
     print("Dados criados com sucesso")
 except:
   print('Dados não inseridos')
 
 try:
-  def cadastrarDonoVeiculo(donoVeiculoNovo):
-    idCliente, idViculo = list(donoVeiculoNovo.values())
-    sql = "INSERT INTO `traba_db`.`tbl_dono_veiculo` (`idCliente`, `idVeiculo`) VALUES(%s, %s)"
-    vlr = (idCliente, idViculo)
+  def verificaLogin(valores):
+    sql = "SELECT * from `traba_db`.`tbl_cliente` where email=%s and senha=%s"
+    vlr = (valores['email'], valores['senha'])
 
     mycursor.execute(sql, vlr)
+    print("Valores da base {}".format(mycursor.fetchone()))
+    terminaConexao()
+except:
+  print('Dados não inseridos')
 
-    print("Dados criados com sucesso")
+try:
+  def cadastrarUsuario(valores):
+    sql = "INSERT INTO `traba_db`.`tbl_usuario` (`nome`, `email`, `senha`) VALUES (%s, %s, %s)"
+    vlr = (valores['nome'], valores['email'], valores['senha'])
+    print("Valores da base {}".format(vlr))
+    mycursor.execute(sql, vlr)
+    terminaConexao()
+except:
+  print('Dados não inseridos')
+
+try:
+  def recuperarSenha(valores):
+    sql = "UPDATE `traba_db`.`tbl_usuario` SET senha=%s where id=%s"
+    vlr = (valores['nome'], valores['email'], valores['senha'])
+    print("Valores da base {}".format(vlr))
+    mycursor.execute(sql, vlr)
+    terminaConexao()
+except:
+  print('Dados não inseridos')
+
+try:
+  def recuperarSenha_verificaEmail(valores):
+    sql = "SELECT * from `traba_db`.`tbl_usuario` where email=%s"
+    vlr = (valores['email'])
+
+    mycursor.execute(sql, vlr)
+    print("Valores da base {}".format(mycursor.fetchone()))
+    terminaConexao()
 except:
   print('Dados não inseridos')
 
